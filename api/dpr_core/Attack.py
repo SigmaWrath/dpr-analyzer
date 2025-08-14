c503a9284dbcb7230bef713300a3281519b74c9a
Sumrath Kaur <sumrath.pahwa@gmail.com>
2025-08-14 02:39:22 -0700

    3D graphing, customizable analysis

--- api/dpr_core/Attack.py
+++ api/dpr_core/Attack.py
@@ -1,5 +1,3 @@
-from numpy.ma.testutils import assert_almost_equal
-
 from dpr_core.DiceFormula import DiceFormula
 
 
@@ -16,17 +14,10 @@ class Attack:
         # If isAC is False, they make a saving throw
         #   ——so we assume we hit 50% of the time
 
-        ### TEST. This is an assertion that must always be true, but is costly.
-        # Delete after some time verifying that it doesn't throw an error.
-        exp_avg = 0
-        for roll in self.raw_damage_dist:
-            exp_avg += roll*self.raw_damage_dist[roll]
-        assert_almost_equal(exp_avg, self.raw_damage_avg, decimal=4,
-                            err_msg="Damage formula averages don't match. Issue with DiceFormula class.")
-
     def __str__(self):
         return self.name
 
+    # Return the probability for the attack to hit a given AC
     def prob_to_hit(self, AC):
         if self.isAC:
             probability = 0
@@ -37,11 +28,13 @@ class Attack:
             probability = 0.5
         return probability
 
+    # Return the damage distribution of the attack against a given AC
     def damage_dist(self, AC):
         hit_prob = self.prob_to_hit(AC)
         result = {key:value*hit_prob for key, value in self.raw_damage_dist.items()}
         result[0] = 1 - hit_prob
         return result
 
+    # Return the average damage of the attack against a given AC
     def damage_avg(self, AC):
         return self.raw_damage_avg*self.prob_to_hit(AC)
--- api/tests/tests_Analyzer.py
+++ api/tests/tests_Analyzer.py
@@ -12,9 +12,9 @@ class MyTestCase(unittest.TestCase):
                                  damagef="1d6+4+1d4", toHitf="1d20+3+4")
         Kalki.add(scimitar_quarry, 2)
 
-        Kalki.compute_avg(10)
+        Kalki.get_avg(10)
 
-    def test_graph_sim_1(self):
+    def test_graph_cross_section_1(self):
         Kalki = Analyzer("Kalki Level 5")
         scimitar_quarry = Attack("Scimitar w/ Ranger's Quarry",
                                  damagef="1d6+4+1d4", toHitf="1d20+3+4")
@@ -42,7 +42,20 @@ class MyTestCase(unittest.TestCase):
         Kalki.add(frighten, 1)
 
         Kalki.analyze(10, 25)
-        Kalki.graph_cross_section(15, color='cyan')
+
+    def test_analyze_3(self):
+        longbow1 = Attack(name="Sharpshoot Longbow w/ Guided Strike",
+                          damagef="1d8+7+1d6+10", toHitf="1d20+17")
+        longbow2 = Attack(name="Sharpshoot Longbow",
+                          damagef="1d8+7+1d6+10", toHitf="1d20+7")
+        frighten = Attack(name="Dread Ambusher: Frighten", damagef="1d8", toHitf="", isAC=False)
+
+        Kalki = Analyzer("Longbow w/ action surge arrows")
+        Kalki.add(longbow1, 1)
+        Kalki.add(longbow2, 3)
+        Kalki.add(frighten, 1)
+
+        Kalki.analyze(10, 25, graphs='3d', testAC=20)
 
 if __name__ == '__main__':
     unittest.main()
